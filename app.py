02cb1493d8092d468562fc29303720a2a4fabc6d
David Carmichael <carmichaelits@gmail.com>
2023-05-24 13:55:15 +0100

    Added database setup example

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-
-# importing module
-from routes import all_routes
-
-app = Flask(__name__)
 """
 Flask will look for the folders called "templates" and "static" in the root folder of the project automatically.
 
 You can change the name of the folders Flask looks for by doing:
 app = Flask(__name__, template_folder="other_templates", static_folder="other_static")
 """
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# importing module
+from routes import all_routes
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 # Passing app above to all_routes function in routes.py module
 all_routes(app)
